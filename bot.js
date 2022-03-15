e3300c6e03d9c232741df8f47068a6128ad4dcba
Enes Kerem AYDIN <46195766+EnesKeremAYDIN@users.noreply.github.com>
2022-03-15 22:36:23 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,15 @@
+var SteamUser = require("steam-user");
+var SteamTotp = require("steam-totp");
+var client = new SteamUser();
+
+client.logOn({
+  accountName: "accountName",
+  password: "password",
+  //twoFactorCode: SteamTotp.generateAuthCode("if_you_have_twoFactorCode")
+});
+client.on("loggedOn", function() {
+  console.log("Bot logged on!");
+  client.setPersona(1);
+  var gamesToIdle = [/*APP id list*/];
+  client.gamesPlayed(gamesToIdle);
+});
